28352e86f5326bbbdce3fa97449ffc79b5f6e5d6
Dennis Nguyen <dennis.nguyen9925@gmail.com>
2023-06-27 21:38:49 -0700

    Complete Array Challenge.

---
+++ prep-array-challenge/src/last.js
@@ -0,0 +1,10 @@
+/* exported last */
+
+function last(array){
+  if (array.length > 0) {
+    return array[array.length - 1];
+  }
+  else {
+    return undefined;
+  }
+};
---
+++ prep-array-challenge/src/tail.js
@@ -0,0 +1,9 @@
+/* exported tail */
+
+function tail(array){
+  let tailArray = [];
+  for (let i = 1; i < array.length; i++){
+    tailArray.push(array[i]);
+  }
+  return tailArray;
+};
